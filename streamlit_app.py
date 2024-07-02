0b77513b12fb86cee80b067a6adf1e240282287e
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-02 23:21:00 +0200

    Update streamlit_app.py
    last update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -13,8 +15,14 @@ st.write(
 cnx = st.connection("Snwoflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingrendients: ',
@@ -24,6 +32,14 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_chosen + 'nutrition information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -38,11 +54,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 
 
