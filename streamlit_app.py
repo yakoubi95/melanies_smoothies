1f8b5f4183a0acc812af3c8d6351e7b07917583b
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-02 22:33:18 +0200

    Update streamlit_app.py
    adding new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
 
