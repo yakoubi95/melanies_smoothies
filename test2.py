a1bdae499767367a0f25f21c839c6304e4daa06f
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-03 19:54:01 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,11 +3,17 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd 
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# convert snow to pandas, so we can use iloc function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Write directly to the app
