7d98a22a3daf06d6ea996067f5981c491c127a42
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-03 19:17:36 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -27,6 +28,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -41,11 +45,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
 
 
