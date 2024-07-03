cdbde16077e58cc59cbe5ac81cf13a88810fd962
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-03 19:12:20 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -44,8 +44,8 @@ if ingredients_list:
 # New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruity_response.json(), use_container_width=True)
 
 
 
