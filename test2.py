13c8223bed72fcf9d376e7419e5c2026df5fba72
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-03 19:57:40 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -13,8 +13,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 
 # convert snow to pandas, so we can use iloc function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -35,6 +35,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon", fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
