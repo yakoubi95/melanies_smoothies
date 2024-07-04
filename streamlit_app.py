b954c638ea44a62d7c3f584a361b8a6d5323a729
yakoubi95 <124059253+yakoubi95@users.noreply.github.com>
2024-07-04 15:07:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-           values ('""" + ingredients_string + """"','""" + name_on_order + """')"""
+           values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
     #st.stop()
